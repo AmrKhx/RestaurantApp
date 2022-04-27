b2c55dff84c74eb55d1be2a11b94a84379a1fd67
AmrKhx <102529176+AmrKhx@users.noreply.github.com>
2022-04-28 00:46:01 +0200

    final1

---
+++ app/src/main/java/com/example/recycleviewproject1/KabajeesActivityActivity.java
@@ -0,0 +1,14 @@
+package com.example.recycleviewproject1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KabajeesActivityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kabajees_activity);
+    }
+}
---
+++ app/src/main/java/com/example/recycleviewproject1/ReportActivity.java
@@ -0,0 +1,14 @@
+package com.example.recycleviewproject1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.report_activity);
+    }
+}
