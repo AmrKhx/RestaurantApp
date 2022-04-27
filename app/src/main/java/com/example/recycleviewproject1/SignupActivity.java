f560fa2d77fa84c19f86d18242f2f96c8253cbf4
AmrKhx <102529176+AmrKhx@users.noreply.github.com>
2022-04-28 01:45:22 +0200

    final11

--- app/src/main/java/com/example/recycleviewproject1/SignupActivity.java
+++ app/src/main/java/com/example/recycleviewproject1/SignupActivity.java
@@ -2,13 +2,25 @@ package com.example.recycleviewproject1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SignupActivity extends AppCompatActivity {
-
+ImageButton signingoin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+        signingoin=(ImageButton)findViewById(R.id.goSignin);
+        signingoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(SignupActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
