dcf44a8170a620284868deb22d5299ddd8332680
AmrKhx <102529176+AmrKhx@users.noreply.github.com>
2022-04-28 02:27:28 +0200

    final11

--- app/src/main/java/com/example/recycleviewproject1/SignInActivity.java
+++ app/src/main/java/com/example/recycleviewproject1/SignInActivity.java
@@ -7,19 +7,23 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignInActivity extends AppCompatActivity  {
 ImageButton signinbtn;
 TextView signupage;
+SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
+        sharedPreferences=getSharedPreferences("my prefs",Context.MODE_PRIVATE);
         signinbtn=(ImageButton) findViewById(R.id.signbtn);
         signupage=(TextView)findViewById(R.id.signup);
         signupage.setOnClickListener(new View.OnClickListener() {
